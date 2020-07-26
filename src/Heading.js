4098cb1627f8eaf232d98d3addaa0a2cb38ea25c
Natalia <n5@mail.ru>
2020-07-26 23:15:31 +0100

    Extracted header to be a separate component

---
+++ src/Heading.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <img
+          className="logoImage"
+          src="https://image.flaticon.com/icons/svg/139/139899.svg"
+          alt="logoImg"
+        />
+        <p className="logoHeading">CYF Hotel</p>
+      </header>
+    </div>
+  );
+};
+
+export default Heading;
